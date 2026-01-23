cb8f3994dbffeb39713d17a172828d8424b8ad1b
Alejandro Mattias <mattias.alejandro@gmail.com>
2026-01-23 12:57:32 -0300

    fix base url to deply url

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// vite.config.js
-export default defineConfig({
-  plugins: [react()],
-  base: '/',  // ← Solo este cambio
-  build: {
-    outDir: 'dist'
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  // Carga las variables del archivo .env
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    base: '/',
+    build: {
+      outDir: 'dist'
+    },
+    define: {
+      // Esto mapea process.env.API_URL al valor de tu .env o de Vercel
+      'process.env.API_URL': JSON.stringify(env.API_URL)
+    }
   }
 });
